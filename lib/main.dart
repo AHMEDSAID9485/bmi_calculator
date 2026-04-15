8404d894f42f859a40d356f749301df76cb80bdf
AHMEDSAID9485 <ahmedascahmed@gmail.com>
2026-04-15 05:24:21 +0200

    First Commit - BMI App

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:bmi/pages/SplachScreen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Bmi());
+}
+
+class Bmi extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplachScreen(),
+    );
+  }
+}
